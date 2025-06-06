3039fb404dcdefc005dfae0b66b4a53c2adadf37
OmRana2004 <omrana8755800322@gmail.com>
2025-06-06 23:56:40 +0530

    exprss

--- week18_SQL/src/index.ts
+++ week18_SQL/src/index.ts
@@ -1,4 +1,9 @@
+import express from "express";
 import { Client } from "pg";
+
+const app = express();
+app.use(express.json());
+
 
 const pgClint = new Client("postgresql://neondb_owner:npg_4vHwdMKTBr3m@ep-solitary-hall-a8psmub3-pooler.eastus2.azure.neon.tech/neondb?sslmode=require");
 
