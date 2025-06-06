5d3a68bf94b16a02bcc85f8ae8d828ff9ea8fda4
OmRana2004 <omrana8755800322@gmail.com>
2025-06-07 00:16:32 +0530

    express: add SQL query to get the top 10 most expensive products

--- week18_SQL/dist/index.js
+++ week18_SQL/dist/index.js
@@ -8,14 +8,34 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
 const pg_1 = require("pg");
-const pgClint = new pg_1.Client("postgresql://neondb_owner:npg_4vHwdMKTBr3m@ep-solitary-hall-a8psmub3-pooler.eastus2.azure.neon.tech/neondb?sslmode=require");
-function main() {
-    return __awaiter(this, void 0, void 0, function* () {
-        yield pgClint.connect();
-        const response = yield pgClint.query("UPDATE users SET username= 'Hanuman' WHERE id=2");
-        console.log(response.rows);
-    });
-}
-main();
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+const pgClient = new pg_1.Client("postgresql://neondb_owner:npg_4vHwdMKTBr3m@ep-solitary-hall-a8psmub3-pooler.eastus2.azure.neon.tech/neondb?sslmode=require");
+pgClient.connect();
+app.post("/signup", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const username = req.body.username;
+    const password = req.body.password;
+    const email = req.body.email;
+    try {
+        const insertQuery = `INSERT INTO users (username, password, email) VALUES ($1, $2, $3);`;
+        const response = yield pgClient.query(insertQuery, [username, password, email]);
+        res.json({
+            message: "You have successfully signed up",
+        });
+    }
+    catch (error) {
+        console.error("Error inserting data:", error);
+        res.status(500).json({
+            error: "Failed to sign up. Please try again.",
+        });
+    }
+}));
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
--- week18_SQL/src/index.ts
+++ week18_SQL/src/index.ts
@@ -4,14 +4,32 @@ import { Client } from "pg";
 const app = express();
 app.use(express.json());
 
+const pgClient = new Client("postgresql://neondb_owner:npg_4vHwdMKTBr3m@ep-solitary-hall-a8psmub3-pooler.eastus2.azure.neon.tech/neondb?sslmode=require");
 
-const pgClint = new Client("postgresql://neondb_owner:npg_4vHwdMKTBr3m@ep-solitary-hall-a8psmub3-pooler.eastus2.azure.neon.tech/neondb?sslmode=require");
+pgClient.connect();
+    
+app.post("/signup", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const email = req.body.email;
+    try {
+        const insertQuery = `INSERT INTO users (username, password, email) VALUES ($1, $2, $3);`
 
-async function main() {
-    await pgClint.connect();
-    const response = await pgClint.query("UPDATE users SET username= 'Hanuman' WHERE id=2");
-    console.log(response.rows);
-}
+        const response = await pgClient.query(insertQuery, [username, password, email]);
+        res.json({
+            message: "You have successfully signed up",
+    })
+    } catch (error) {
+        console.error("Error inserting data:", error);
+         res.status(500).json({
+      error: "Failed to sign up. Please try again.",
+         });
+    }
+});
 
-main()
+app.listen(3000, () =>{
+    console.log("Server is running on port 3000");
+})
+
+
 
