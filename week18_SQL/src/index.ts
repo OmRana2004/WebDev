cd654a370af7490be147a27d7ff4838aaf05c9b1
OmRana2004 <omrana8755800322@gmail.com>
2025-06-06 23:34:51 +0530

    UPADTING week18_SQL dist and package.json

--- week18_SQL/src/index.ts
+++ week18_SQL/src/index.ts
@@ -1,6 +1,12 @@
 import { Client } from "pg";
 
-const pgClint = new Client({
-    user: "neondb_owner",
-    
-})
+const pgClint = new Client("postgresql://neondb_owner:npg_4vHwdMKTBr3m@ep-solitary-hall-a8psmub3-pooler.eastus2.azure.neon.tech/neondb?sslmode=require");
+
+async function main() {
+    await pgClint.connect();
+    const response = await pgClint.query("UPDATE users SET username= 'Hanuman' WHERE id=2");
+    console.log(response.rows);
+}
+
+main()
+
