4d6cbf3a395a64dc263819ad83ea6d64a5d8b64f
OmRana2004 <omrana8755800322@gmail.com>
2025-06-11 23:39:06 +0530

    create a new ws server

--- week-17-ws/src/index.ts
+++ week-17-ws/src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello, World!');
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+// event handlers
+wss.on("connection", function(socket) {
+    console.log("user connected")
+        socket.send("helllo")
+    
+})
