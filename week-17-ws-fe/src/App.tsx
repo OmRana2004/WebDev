4ca0691dcb373289349eb640984fb92ae900db5f
OmRana2004 <omrana8755800322@gmail.com>
2025-06-12 01:18:33 +0530

    errors

--- week-17-ws-fe/src/App.tsx
+++ week-17-ws-fe/src/App.tsx
@@ -1,10 +1,30 @@
-
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [socket, setSocket] = useState();
+
+  function sendMessage() {
+    if (!socket) {
+      return;
+    }
+    // @ts-ignore
+    socket.send("ping")
+  }
+
+ useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8080");
+    setSocket(ws);
+    
+    ws.onmessage = (ev) => {
+      alert(ev.data);
+    }
+  }, []);
+
   return (
     <div>
-      om rana
+      <input type="text" placeholder='Message..' ></input>
+      <button onClick={sendMessage}>Send</button>
     </div>
   )
 }
