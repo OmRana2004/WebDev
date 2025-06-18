e52506728e606f90f2e3ccdfe181ca099a416446
OmRana2004 <omrana8755800322@gmail.com>
2025-06-19 00:20:42 +0530

    hardcoded commit message

--- Week-16 WebSockets (Projet_ChatApp)/Frontend/src/App.tsx
+++ Week-16 WebSockets (Projet_ChatApp)/Frontend/src/App.tsx
@@ -1,35 +1,30 @@
-import { useEffect, useState, useRef } from 'react';
 
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [socket, setSocket] = useState();
-  const inputRef = useRef();
-  function sendMessage() {
-    if (!socket) {
-      return;
-    }
-    const message = inputRef.current.value;
-    // @ts-ignore
-    socket.send(message)
-  }
+  const [messages, setMessages] = useState(["hi there", "how are you?"]);
 
- useEffect(() => {
-    const ws = new WebSocket("ws://localhost:8080");
-    setSocket(ws);
-    
-    ws.onmessage = (ev) => {
-      alert(ev.data);
+  useEffect(() => {
+    const ws = new WebSocket('http://localhost:3000');
+    ws.onmessage = (event) => {
+      setMessages(m => [...m, event.data]);
     }
   }, []);
-
+   
   return (
-    <div>
-      <input ref={inputRef} type="text" placeholder='Message...' ></input>
-      <button onClick={sendMessage}>Send</button>
+    <div  className='h-screen bg-black '>
+      <div className='h-[95vh]'>
+        {messages.map(message => <div>{message} </div>)}
+      </div>
+      <div className='w-full bg-white flex p-4'>
+        <input className='flex-1'></input>
+        <button className='bg-purple-600 text-white p-4'>
+          Send Message
+        </button>
+      </div>
     </div>
   )
 }
 
 export default App;
-
