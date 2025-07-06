f4c21b5a5ccd581a6ee9feaa8d33218378b812bd
OmRana2004 <omrana8755800322@gmail.com>
2025-07-07 00:55:21 +0530

    adding sql queery to update todo table using prisma orams

--- week18-Prisma/dist/index.js
+++ week18-Prisma/dist/index.js
@@ -1,2 +1,26 @@
 "use strict";
-console.log("hello world!");
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const client_1 = require("@prisma/client");
+const clint = new client_1.PrismaClient();
+function createUser() {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield clint.user.create({
+            data: {
+                username: "omRana",
+                password: "123456",
+                age: 20,
+                city: "Uttarkashi",
+            }
+        });
+    });
+}
+createUser();
--- week18-Prisma/src/index.ts
+++ week18-Prisma/src/index.ts
@@ -1,1 +1,17 @@
-console.log("hello world!")
+import { PrismaClient } from '@prisma/client';
+
+const clint = new PrismaClient();
+
+
+async function createUser() {
+    await clint.user.create({
+    data: {
+        username: "omRana",
+        password: "123456",
+        age: 20,
+        city: "Uttarkashi",
+    }
+})
+}
+createUser();
+
