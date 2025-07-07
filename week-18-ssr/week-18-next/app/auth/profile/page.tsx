bb4cffba65cc76aeba32c517bdc6d6866298a6be
OmRana2004 <omrana8755800322@gmail.com>
2025-07-07 22:17:25 +0530

    palying with layoutes and components

---
+++ week-18-ssr/week-18-next/app/auth/profile/page.tsx
@@ -0,0 +1,9 @@
+export default function Profile() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <p className="text-xl font-bold text-white hover:text-green-500 transition-colors duration-300 cursor-pointer">
+        Profile page created by OM RANA
+      </p>
+    </div>
+  );
+}
