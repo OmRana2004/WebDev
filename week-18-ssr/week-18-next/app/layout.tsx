c68e5730f1b1bd83eac5ffff4a3dc13f98fa2209
OmRana2004 <omrana8755800322@gmail.com>
2025-07-08 21:35:42 +0530

    ./git/COMMIT_EDITMSG: commit message

--- week-18-ssr/week-18-next/app/layout.tsx
+++ week-18-ssr/week-18-next/app/layout.tsx
@@ -33,5 +33,4 @@ export default function RootLayout({
     </html>
   );
 }
-// componentes are used to store a resuable contact informations to reduces to collective code. what are World State in ETH.
-//is ues dto cpllective code and to reuses the code. by functionally 
+// componentes are used to store a resuable contact informations to reduces to collective code. what are World State in ET
