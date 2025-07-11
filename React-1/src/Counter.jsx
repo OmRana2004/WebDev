7ea612844eaa787143c9c21d82854b807beca164
OmRana2004 <omrana8755800322@gmail.com>
2025-07-11 12:12:40 +0530

    Counter App

--- React-1/src/Counter.jsx
+++ React-1/src/Counter.jsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
 
 function Counter() {
-    // 1️⃣ count naam ka ek state banaya jisme initial value 0 hai.
     const [count, setCount] = useState(0);
     return (
         <div>
             <h2>Count: {count}</h2>
-            <button onClick={() => setCount(count + 1)}>Increase count</button>
+            <button onClick={() => setCount(count+1)}>Increase Count</button>
         </div>
     )
 }
