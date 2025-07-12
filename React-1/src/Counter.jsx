7d0e75c856cfe1b8b412bccedd82768d24c6e1bf
OmRana2004 <omrana8755800322@gmail.com>
2025-07-12 14:03:33 +0530

    ./

--- React-1/src/Counter.jsx
+++ React-1/src/Counter.jsx
@@ -1,25 +1,16 @@
 import { useState } from "react";
 
-function Counter() {
-    const [count, setCount] = useState(0);
-<<<<<<< HEAD
-    return (
-        <div>
-            <h2>Count:{count}</h2>
-            <button onClick={()=>}>Increase Value</button>
-        </div>
-    )
-}
-=======
+function Counter () {
 
+    const [count, setCount] = useState(0);
     return (
         <div>
             <h2>Count:{count}</h2>
-            <button onClick={()=> setCount(count+1)}>Increase Value</button>
-            <button onClick={()=> setCount(count-1)}>Decereese Value</button>
+            <button onClick={() => setCount (count+1)}>Increace Count</button>
+            
+            <button onClick={() => setCount (count-1)}>Decerese Count</button>
         </div>
     )
 }
 
 export default Counter;
->>>>>>> 4ea699b0bf3d19f4b42173b714b078b004fdbb3b
