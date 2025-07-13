6106449b38fe6576d8d18549a7ab1ed4bbb3c3f2
OmRana2004 <omrana8755800322@gmail.com>
2025-07-13 14:20:54 +0530

    props and children

--- React-1/src/App.jsx
+++ React-1/src/App.jsx
@@ -5,17 +5,21 @@ import Error_Boundary from './Error_Boundary'
 import './App.css'
 import UserData from './hooks/useEffect/UserData'
 import Cr from './cr/CR'
+import Props from './props_children/Props'
+import Card from './props_children/Card'
 
 
 function App() {
   
   return (
       <div>
-        <Counter />
+        {/* <Counter /> */}
         {/* <UserData /> */}
         {/* <LinkedinTopbar /> */}
         {/* <Error_Boundary /> */}
-        <Cr />  
+        {/* <Props title="iPhone" price="10000" /> */}
+        {/* <Cr />   */}
+        <Card />
       </div>
   )
 }
---
+++ React-1/src/props_children/Props.jsx
@@ -0,0 +1,11 @@
+const Props = ({ title, price }) => {
+    return(
+        <div> 
+            <h2>{title}</h2>
+            <p>Price: {price}</p>
+        </div>
+        
+    )
+}
+
+export default Props;
