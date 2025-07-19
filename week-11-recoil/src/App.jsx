4e18cfbddd28454f7f044c80e80cd1568ae786b9
OmRana2004 <omrana8755800322@gmail.com>
2025-07-20 01:12:00 +0530

    Atom And Selector.

---
+++ week-11-recoil/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect, memo } from "react";
+import "./App.css";
+import { RecoilRoot, useRecoilValue, useSetRecoilState } from "recoil";
+import { counterAtom, evenSelector } from "./counter";
+
+function App() {
+  return (
+    <div>
+      <RecoilRoot>
+      <Buttons />
+      <Counter />
+      <IsEven />
+      </RecoilRoot>
+    </div>
+  );
+}
+
+function Buttons() {
+  const setCount = useSetRecoilState(counterAtom);
+
+  function increase() {
+    setCount((c) => c + 2);
+  }
+
+  function decrease() {
+    setCount((c) => c - 1);
+  }
+
+  return (
+    <div>
+      <button onClick={increase}>Increase</button>
+      <button onClick={decrease}>Decrease</button>
+    </div>
+  );
+}
+
+function Counter() {
+  const count = useRecoilValue(counterAtom);
+
+  return <div>{count}</div>;
+}
+
+function IsEven() {
+  const even = useRecoilValue(evenSelector);
+
+  return <div>{even ? "Even" : "Odd"}</div>;
+}
+
+// is function is called by its instances and is to reduces and code by its values.
+
+export default App;
