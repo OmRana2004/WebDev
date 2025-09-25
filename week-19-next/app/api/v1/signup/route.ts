2b267f0678931673b3ab5f3a482bd68c7cf4dc3a
OmRana2004 <omrana8755800322@gmail.com>
2025-09-25 22:24:55 +0530

    data added sucessfully in database     return NextResponse.json({ message: "User created

--- week-19-next/app/api/v1/signup/route.ts
+++ week-19-next/app/api/v1/signup/route.ts
@@ -1,13 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+
+
+const prismaClient = new PrismaClient();
 
 export async function POST(req: NextRequest) {
+  const data = await req.json();
+  
+  await prismaClient.user.create({
+    data: {
+      username: data.username,
+      password: data.password,
+    },
+  });
 
-
-    const data = await req.json();
-
-    console.log(data);
-   
-    return NextResponse.json({
-        message: "You have signed up successfully!"
-    })
+  return NextResponse.json({
+    message: "You have signed up successfully!",
+  });
 }
