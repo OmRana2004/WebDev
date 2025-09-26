bccc03b3acf57ac38eed172b6b819ac771b51f76
OmRana2004 <omrana8755800322@gmail.com>
2025-09-26 23:43:51 +0530

    dynamic route [blogId]

---
+++ week-20-next/app/blog/[blogId]/page.tsx
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+ export default async function BlogPage({params}: any) {
+   const postId = params.blogId;
+   const res = await axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`);
+   const data = res.data;
+
+  return (
+    <div>
+      Title - {data.title}
+        <br />
+        Description - {data.body}
+    </div>
+  )
+}
