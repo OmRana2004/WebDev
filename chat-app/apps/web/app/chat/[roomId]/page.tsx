2f1ed6f8a008cf234150f4f60c26bb38faed2b5b
OmRana2004 <omrana8755800322@gmail.com>
2025-10-09 22:18:51 +0530

    ..

---
+++ chat-app/apps/web/app/chat/[roomId]/page.tsx
@@ -0,0 +1,19 @@
+import { TextInput } from "@repo/ui/text-input";
+
+export default function() {
+    return <div style={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        justifyContent: "space-between",
+        flexDirection: "column"
+        }}>
+            <div>
+                Chat Room
+            </div>
+            <div>
+                <TextInput size="big" placeholder="Chat here"></TextInput>
+            </div>
+        
+        </div>
+}
