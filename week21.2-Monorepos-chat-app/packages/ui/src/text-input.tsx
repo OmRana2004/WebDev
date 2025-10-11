9457a383ff0e0b4b4770ba41671491fdbcc26d3e
OmRana2004 <omrana8755800322@gmail.com>
2025-10-11 11:12:07 +0530

    recaping

--- week21.2-Monorepos-chat-app/packages/ui/src/text-input.tsx
+++ week21.2-Monorepos-chat-app/packages/ui/src/text-input.tsx
@@ -1,17 +1,17 @@
 interface PropType {
-    placeholder: string
-    size: "big" | "small"
+    placeholder: string;
+    size: "big" | "small";
 }
 
-
-export function TextInput({ 
+export default function TextInput({
     placeholder,
     size
- }: PropType) {
+}: PropType) {
     return <input placeholder={placeholder} style={{
         padding: size === "big" ? 20 : 10,
         margin: size === "big" ? 20 : 10,
         borderColor: "black",
-        borderWidth: 1
-    }}></input>
+        borderWidth: 1,
+    }}>   
+    </input>
 }
