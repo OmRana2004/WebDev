26c840d26adb80a1b8ae8190eb86cfcaac968172
OmRana2004 <omrana8755800322@gmail.com>
2026-06-30 00:35:11 +0530

    revide

---
+++ React-Recape/src/App.tsx
@@ -0,0 +1,27 @@
+export default function App() {
+  return (
+    <div className="bg-amber-800 min-h-screen p-4 flex">
+      {/* Main Content Card Container */}
+      <div className="bg-amber-300 flex-1 flex flex-col gap-4 p-4 rounded-xl shadow-xl">
+        
+        {/* Row Items */}
+        <div className="flex-1 flex items-center justify-center bg-red-50 font-semibold rounded-lg text-slate-800 ">
+          Hi Sir!
+        </div>
+        
+        <div className="flex-1 flex items-center justify-center bg-red-50 font-semibold rounded-lg text-slate-800">
+          Hi Sir!
+        </div>
+        
+        <div className="flex-1 flex items-center justify-center bg-red-50 font-semibold rounded-lg text-slate-800">
+          Hi Sir!
+        </div>
+        
+        <div className="flex-1 flex items-center justify-center bg-red-50 font-semibold rounded-lg text-slate-800">
+          Hi Sir!
+        </div>
+
+      </div>
+    </div>
+  )
+}
